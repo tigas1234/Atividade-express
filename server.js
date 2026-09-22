bb60eb690833cf5e0908799b61ff36080b0a5e76
Tiago dos Santos Valadares <tiagovaladares128@gmail.com>
2026-09-22 09:56:50 -0300

    adicionado criação de tarefas

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 let tarefas = [
-{ id: 1, titulo: "Estudar Express", concluida: false },
-{ id: 2, titulo: "Fazer atividade", concluida: true },
-{ id: 3, titulo: "Testar API", concluida: false }
-];
+    { id: 1, titulo: "Estudar Express", concluida: false },
+    { id: 2, titulo: "Fazer atividade", concluida: true },
+    { id: 3, titulo: "Testar API", concluida: false }];
 
 app.get("/", (req, res) => {
 res.send("API de Tarefas no ar");});
@@ -19,6 +20,7 @@ return res.json(tarefas.filter(tarefa => tarefa.concluida === true));}
 
 if (concluida === "false") {
 return res.json(tarefas.filter(tarefa => tarefa.concluida === false));}
+
 res.json(tarefas);});
 
 app.get("/tarefas/:id", (req, res) => {
@@ -28,7 +30,15 @@ const tarefa = tarefas.find(tarefa => tarefa.id === id);
 if (!tarefa) {
 return res.status(404).json({
 erro: "Tarefa não encontrada"});}
+
 res.json(tarefa);});
+
+app.post("/tarefas", (req, res) => {
+const { titulo } = req.body;
+const novaTarefa = {id: tarefas.length + 1, titulo: titulo, concluida: false};
+
+tarefas.push(novaTarefa);
+res.status(201).json(novaTarefa);});
 
 app.listen(PORT, () => {
 console.log(`Servidor rodando na porta ${PORT}`);});
