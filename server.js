a1ebd041972c26e662e5056702996a04bd5921f0
Tiago dos Santos Valadares <tiagovaladares128@gmail.com>
2026-09-22 09:49:14 -0300

    adicionado busca de tarefa por id

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 3000;
 
 let tarefas = [
-{id: 1, titulo: "Estudar Express", concluida: false},
-{id: 2, titulo: "Fazer atividade", concluida: true},
-{id: 3,titulo: "Testar API", concluida: false}
+{ id: 1, titulo: "Estudar Express", concluida: false },
+{ id: 2, titulo: "Fazer atividade", concluida: true },
+{ id: 3, titulo: "Testar API", concluida: false }
 ];
 
 app.get("/", (req, res) => {
@@ -13,6 +13,16 @@ app.get("/", (req, res) => {
 
 app.get("/tarefas", (req, res) => {
     res.json(tarefas);});
+
+app.get("/tarefas/:id", (req, res) => {
+const id = Number(req.params.id);
+const tarefa = tarefas.find(tarefa => tarefa.id === id);
+
+if (!tarefa) {
+return res.status(404).json({
+erro: "Tarefa não encontrada"});}
+
+    res.json(tarefa);});
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);});
