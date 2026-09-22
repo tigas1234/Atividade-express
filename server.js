46a6298bd916767ae639aec4d82de6b4c72e9bd3
Tiago dos Santos Valadares <tiagovaladares128@gmail.com>
2026-09-22 09:44:13 -0300

    adicionado rota de tarefas

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
 
+let tarefas = [
+{id: 1, titulo: "Estudar Express", concluida: false},
+{id: 2, titulo: "Fazer atividade", concluida: true},
+{id: 3,titulo: "Testar API", concluida: false}
+];
+
 app.get("/", (req, res) => {
-    res.send("API de Tarefas no ar");
-});
+    res.send("API de Tarefas no ar");});
+
+app.get("/tarefas", (req, res) => {
+    res.json(tarefas);});
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+    console.log(`Servidor rodando na porta ${PORT}`);});
